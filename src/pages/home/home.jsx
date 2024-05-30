889833cd12c42076f03ef9f7491cfd06e584e102
Gabriel MARIE <gaby01@youmail.fr>
2024-05-30 17:23:11 +0200

    adding

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -5,12 +5,14 @@ import FeatureComponent from "../../components/feature.jsx";
 import NavbarComponent from "../../components/navbar.jsx";
 import MainInfoComponent from "../../components/mainInfo.jsx";
 import FooterComponent from "../../components/footer.jsx";
+import BentoInfoComponent from "../../components/bentoInfos.jsx";
 
 const Home = () => {
 	return (
 		<>
 			<NavbarComponent></NavbarComponent>
 			<MainInfoComponent></MainInfoComponent>
+			<BentoInfoComponent></BentoInfoComponent>
 			<FooterComponent></FooterComponent>
 		</>
 	);
