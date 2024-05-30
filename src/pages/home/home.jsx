4d940f147f59ae397463a1526cf4a8844d83712e
Gabriel MARIE <gaby01@youmail.fr>
2024-05-30 17:07:58 +0200

    footer done

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -4,14 +4,16 @@ import PricingComponent from "../../components/princing.jsx";
 import FeatureComponent from "../../components/feature.jsx";
 import NavbarComponent from "../../components/navbar.jsx";
 import MainInfoComponent from "../../components/mainInfo.jsx";
+import FooterComponent from "../../components/footer.jsx";
 
 const Home = () => {
-  return (
-    <>
-      <NavbarComponent></NavbarComponent>
-      <MainInfoComponent></MainInfoComponent>
-    </>
-  );
+	return (
+		<>
+			<NavbarComponent></NavbarComponent>
+			<MainInfoComponent></MainInfoComponent>
+			<FooterComponent></FooterComponent>
+		</>
+	);
 };
 
 export default Home;
