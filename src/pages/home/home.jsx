d56acd4c8a823d229c67740a30b1ad91cf93de62
Gabriel MARIE <gaby01@youmail.fr>
2024-05-30 16:30:29 +0200

    adding mainInfo component

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -3,11 +3,13 @@ import React from "react";
 import PricingComponent from "../../components/princing.jsx";
 import FeatureComponent from "../../components/feature.jsx";
 import NavbarComponent from "../../components/navbar.jsx";
+import MainInfoComponent from "../../components/mainInfo.jsx";
 
 const Home = () => {
   return (
     <>
       <NavbarComponent></NavbarComponent>
+      <MainInfoComponent></MainInfoComponent>
     </>
   );
 };
