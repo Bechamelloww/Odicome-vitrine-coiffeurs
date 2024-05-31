ec776ed9596d7ea102cf596dd34685599d5dff06
thomasperge <thomaskauffmant74@gmail.com>
2024-05-31 15:59:36 +0200

    fix: fix bento padding

--- src/components/bentoInfos.jsx
+++ src/components/bentoInfos.jsx
@@ -30,21 +30,24 @@ const BentoInfoComponent = () => {
         }
     ]
     return (
-        <div className="grid auto-rows-[auto] grid-cols-3 gap-4">
-            {[...Array(5)].map((_, i) => (
-                // <div
-                //     key={i}
-                //     className={`row-span-1 rounded-xl border-2 border-slate-400/10 bg-neutral-100 p-4 dark:bg-neutral-900 ${i === 3 || i === 6 ? "col-span-2" : ""
-                //         }`}
-                // ></div>
-                <BentoContentComponent
-                    title={data[i].title}
-                    description={data[i].description}
-                    image={data[i].image}
-                    className={`row-span-1 rounded-xl border-2 border-slate-400/10 bg-neutral-100 p-4 dark:bg-neutral-900 ${i === 3 || i === 6 ? "col-span-2" : ""}`}
-                ></BentoContentComponent>
-            ))}
-        </div>
+        <>
+            <h2 className='w-full flex items-center justify-center mt-20 mb-10 font-bold text-2xl'>Des Fonctionnalités Adaptées à Vos Besoins</h2>
+            <div className="grid auto-rows-[auto] grid-cols-3 gap-4 mt-10">
+                {[...Array(5)].map((_, i) => (
+                    // <div
+                    //     key={i}
+                    //     className={`row-span-1 rounded-xl border-2 border-slate-400/10 bg-neutral-100 p-4 dark:bg-neutral-900 ${i === 3 || i === 6 ? "col-span-2" : ""
+                    //         }`}
+                    // ></div>
+                    <BentoContentComponent
+                        title={data[i].title}
+                        description={data[i].description}
+                        image={data[i].image}
+                        className={`row-span-1 rounded-xl border-2 border-slate-400/10 bg-neutral-100 p-4 dark:bg-neutral-900 ${i === 3 || i === 6 ? "col-span-2" : ""}`}
+                    ></BentoContentComponent>
+                ))}
+            </div>
+        </>
     );
 };
 
